dfe6dd8e7f0ed800fc5c7d48d3bc6c138fd5bb62
Dirk Sammel <dirk.sammel@gmail.com>
2022-02-22 15:22:34 +0000

    removed granularity parts from stopper, adjustments after testing with black and flake8

--- cobald_tests/decorator/test_stopper.py
+++ cobald_tests/decorator/test_stopper.py
@@ -4,24 +4,21 @@ from ..mock.pool import FullMockPool
 
 from cobald.decorator.stopper import Stopper
 
-class TestStopper(object):
 
+class TestStopper(object):
     def test_init_enforcement(self):
         pool = FullMockPool()
         with pytest.raises(ValueError):
-            Stopper(pool, partition='test', granularity=-10)
+            Stopper(pool, partition="test", interval=-10)
         with pytest.raises(ValueError):
-            Stopper(pool, partition='test', interval=-10)
-        with pytest.raises(ValueError):
-            Stopper(pool, partition='')
+            Stopper(pool, partition="")
         with pytest.raises(ValueError):
             Stopper(pool)
 
-            
     def test_running(self):
         pool = FullMockPool()
-        stopper = Stopper(pool, partition='test')
-        
+        stopper = Stopper(pool, partition="test")
+
         for pend_jobs in (2, 7, 150, 5000):
             stopper.n_pend_jobs = pend_jobs
             for value in (0, 1, 5, 10, 1000):
@@ -29,10 +26,9 @@ class TestStopper(object):
 
     def test_idle(self):
         pool = FullMockPool()
-        stopper = Stopper(pool, partition='test')
-        
+        stopper = Stopper(pool, partition="test")
+
         stopper.n_pend_jobs = 0
 
         for value in (0, 1, 5, 10, 1000):
             assert stopper._condition_slurm(value) == 0
-
--- src/cobald/decorator/stopper.py
+++ src/cobald/decorator/stopper.py
@@ -7,38 +7,33 @@ import asyncio
 
 from cobald.daemon import service
 
+
 @service(flavour=asyncio)
 class Stopper(PoolDecorator):
     """
-    Decorator that sets the demand to 0 if there are no pending jobs on the partition
+    Decorator that sets demand to 0 if the partition has no pending jobs
 
     :param target: the pool
-    :param granularity: granularity of ``target.demand``
     :param partition: partition that is checked for pending jobs
     :param interval: interval in seconds between checks of ``partition``
 
-    If there are pending jobs on the partition, the demand is not modified. Otherwise, the demand is set to 0 and kept at 0 as long as there no pending jobs are detected.
+    If there are pending jobs on the partition, the demand is not modified.
+    The demand is set to 0 as long as no pending jobs are detected.
 
     The default interval is 300 (5 minutes). The partition has to be specified.
     """
 
     @property
     def demand(self) -> float:
-        if abs(self._demand - self.target.demand) >= self.granularity:
-            self._demand = self.target.demand
         return self._demand
 
     @demand.setter
     def demand(self, value: float):
-        # Record demand
         self._demand = self._condition_slurm(value)
-        if self.granularity != 1:
-            self.target.demand = self._condition_slurm(_floor(value, self.granularity))
-        else:
-            self.target.demand = self._demand
-    
+        self.target.demand = self._demand
+
     def _condition_slurm(self, value):
-        """Set demand to zero if there are no pending jobs, otherwise pass `value`"""
+        """Return 0 if there are no pending jobs, otherwise pass `value`"""
         if self.n_pend_jobs == 0:
             return 0
         else:
@@ -47,12 +42,14 @@ class Stopper(PoolDecorator):
     async def run(self):
         """Retrieve the number of pending jobs on `partition`"""
         while True:
-            proc = subprocess.Popen(f"squeue -p {self.partition} -t pending -h | wc -l",
-                                    shell=True,
-                                    stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE)
+            proc = subprocess.Popen(
+                f"squeue -p {self.partition} -t pending -h | wc -l",
+                shell=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
             stdout, stderr = proc.communicate()
-            self.n_pend_jobs = int(stdout.decode('ascii').strip())
+            self.n_pend_jobs = int(stdout.decode("ascii").strip())
             await asyncio.sleep(self.interval)
 
     # async def run(self):
@@ -60,25 +57,23 @@ class Stopper(PoolDecorator):
     #     while True:
     #         proc = await asyncio.create_subprocess_shell(
     #             f"squeue -p {self.partition} -t pending -h | wc -l",
-    #             stdout=asyncio.subprocess.PIPE, 
-    #             stderr=asyncio.subprocess.PIPE)
+    #             stdout=asyncio.subprocess.PIPE,
+    #             stderr=asyncio.subprocess.PIPE,
+    #         )
     #         stdout, stderr = await proc.communicate()
-    #         self.n_pend_jobs = int(stdout.decode('ascii').strip())
+    #         self.n_pend_jobs = int(stdout.decode("ascii").strip())
     #         await asyncio.sleep(self.interval)
 
     def __init__(
         self,
         target: Pool,
-        granularity: int = 1,
-        partition: str = '',
+        partition: str = "",
         interval: int = 300,
     ):
         super().__init__(target)
-        enforce(granularity > 0, ValueError("granularity must be positive"))
         enforce(interval > 0, ValueError("interval must be positive"))
-        enforce(partition != '', ValueError("partition must be specified"))
+        enforce(partition != "", ValueError("partition must be specified"))
         self._demand = target.demand
-        self.granularity = granularity
         self.partition = partition
         self.interval = interval
         self.n_pend_jobs = 0
